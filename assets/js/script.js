7c2db9b6ca66e1acf3a038fc95610e2e36e722b2
Bishru125 <bisharu@veuz.in>
2025-03-28 13:25:05 +0300

    latest

--- assets/js/script.js
+++ assets/js/script.js
@@ -266,76 +266,13 @@ $(function () {
 
 //   dropdown
 
-const locales = ["en-GB", "ar-SA", "zh-CN", "de-DE", "es-ES", "fr-FR", "hi-IN", "it-IT", "in-ID", "ja-JP", "ko-KR", "nl-NL", "no-NO", "pl-PL", "pt-BR", "sv-SE", "fi-FI", "th-TH", "tr-TR", "uk-UA", "vi-VN", "ru-RU", "he-IL", "en-SG"]; 
-
-function getFlagSrc(countryCode) {
-  return /^[A-Z]{2}$/.test(countryCode)
-       ? `https://flagsapi.com/${countryCode.toUpperCase()}/shiny/64.png`
-       : "";
-}
-
-const dropdownBtn = document.getElementById("dropdown-btn");
-const dropdownContent = document.getElementById("dropdown-content");
-
-function setSelectedLocale(locale) {
-  const intlLocale = new Intl.Locale(locale);
-  
-
-  let langName = new Intl.DisplayNames([locale], {
-    type: "language",
-  }).of(intlLocale.language);
-  
-  if (intlLocale.region) {
-    const regionName = new Intl.DisplayNames([locale], {
-      type: "region",
-    }).of(intlLocale.region);
-    langName += ` (${regionName})`;
-  }
-  
-  // Clear current content
-  dropdownContent.innerHTML = "";
-  
-  const otherLocales = locales.filter((loc) => loc !== locale);
-  otherLocales.forEach((otherLocale) => {
-    const otherIntlLocale = new Intl.Locale(otherLocale);
-    let otherLangName = new Intl.DisplayNames([otherLocale], {
-      type: "language",
-    }).of(otherIntlLocale.language);
-    
-    const listEl = document.createElement("li");
-    listEl.innerHTML = `${otherLangName}<img src="${getFlagSrc(otherIntlLocale.region)}" />`;
-    listEl.value = otherLocale;
-    listEl.addEventListener("mousedown", function () {
-      setSelectedLocale(otherLocale);
-    });
-    dropdownContent.appendChild(listEl);
-  });
-  
-  dropdownBtn.innerHTML = `<img src="${getFlagSrc(intlLocale.region)}" />${langName}<span class="arrow-down"></span>`;
-}
-
-setSelectedLocale("en-SG");
-
-const browserLang = new Intl.Locale(navigator.language).language;
-for (const locale of locales) {
-  const localeLang = new Intl.Locale(locale).language;
-  if (localeLang === browserLang) {
-  }
-}
-
 // currency
 
 
 const currencies = [
+  { code: 'SGD', symbol: 'S$', name: 'Singapore Dollar' },
     { code: 'USD', symbol: '$', name: 'US Dollar' },
-    { code: 'EUR', symbol: '€', name: 'Euro' },
-    { code: 'GBP', symbol: '£', name: 'British Pound' },
-    { code: 'JPY', symbol: '¥', name: 'Japanese Yen' },
-    { code: 'AUD', symbol: 'A$', name: 'Australian Dollar' },
-    { code: 'CAD', symbol: 'C$', name: 'Canadian Dollar' },
-    { code: 'CHF', symbol: 'Fr', name: 'Swiss Franc' },
-    { code: 'CNY', symbol: '¥', name: 'Chinese Yuan' },
-    { code: 'INR', symbol: '₹', name: 'Indian Rupee' },
+    
   ];
   
   class CurrencyDropdown {
